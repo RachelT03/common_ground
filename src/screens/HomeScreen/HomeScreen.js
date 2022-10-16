5c2f7656be490d8a7cec2f1d92eead9ac37997db
Rachel <rt387@cornell.edu>
2022-10-15 20:09:15 -0400

    Implemented Homepage

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,35 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState, Component } from 'react'
+import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View, SafeAreaView } from 'react-native'
 import styles from './styles';
 import { firebase } from '../../firebase/config'
 
 export default function HomeScreen(props) {
-
     const [entityText, setEntityText] = useState('')
     const [entities, setEntities] = useState([])
 
     const entityRef = firebase.firestore().collection('entities')
     const userID = props.extraData.id
 
-    useEffect(() => {
-        entityRef
-            .where("authorID", "==", userID)
-            .orderBy('createdAt', 'desc')
-            .onSnapshot(
-                querySnapshot => {
-                    const newEntities = []
-                    querySnapshot.forEach(doc => {
-                        const entity = doc.data()
-                        entity.id = doc.id
-                        newEntities.push(entity)
-                    });
-                    setEntities(newEntities)
-                },
-                error => {
-                    console.log(error)
-                }
-            )
-    }, [])
+    // useEffect(() => {
+    //     entityRef
+    //         .where("authorID", "==", userID)
+    //         .orderBy('createdAt', 'desc')
+    //         .onSnapshot(
+    //             querySnapshot => {
+    //                 const newEntities = []
+    //                 querySnapshot.forEach(doc => {
+    //                     const entity = doc.data()
+    //                     entity.id = doc.id
+    //                     newEntities.push(entity)
+    //                 });
+    //                 setEntities(newEntities)
+    //             },
+    //             error => {
+    //                 console.log(error)
+    //             }
+    //         )
+    // }, [])
 
     const onAddButtonPress = () => {
         if (entityText && entityText.length > 0) {
@@ -51,42 +50,137 @@ export default function HomeScreen(props) {
         }
     }
 
-    const renderEntity = ({item, index}) => {
-        return (
-            <View style={styles.entityContainer}>
-                <Text style={styles.entityText}>
-                    {index}. {item.text}
-                </Text>
-            </View>
-        )
-    }
-
     return (
-        <View style={styles.container}>
-            <View style={styles.formContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder='Add new entity'
-                    placeholderTextColor="#aaaaaa"
-                    onChangeText={(text) => setEntityText(text)}
-                    value={entityText}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
-                />
-                <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
-                    <Text style={styles.buttonText}>Add</Text>
+        <SafeAreaView>
+            <View style={styles.container}>
+                <View style={styles.headerbox} >
+                    <Text style={styles.header}>Rachel Tong</Text>
+                    <Text style={styles.header}> </Text>
+                    <Text style={styles.header}> </Text>
+                    <img src={"./CORNELL.jpg"}></img>
+                    <Text style={styles.text}> </Text>
+                    <Text style={styles.header}> </Text>
+                    <View style={styles.favbox}>
+                        <div style={styles.cols}>
+                            <img src={"./CORNELL.jpg"}></img><Text style={styles.text}>Appointments</Text>
+                        </div>
+                        <div>
+                            <img src={"./CORNELL.jpg"}></img><Text style={styles.text}>Resources</Text>
+                        </div>
+                        <div>
+                            <img src={"./CORNELL.jpg"}></img><Text style={styles.text}>Upcoming Events</Text>
+                        </div>
+                        <div>
+                            <img src={"./CORNELL.jpg"}></img><Text style={styles.text}>Upcoming Events</Text>
+                        </div>
+                        <div>
+                            <img src={"./CORNELL.jpg"}></img><Text style={styles.text}>Upcoming Events</Text>
+                        </div>
+                    </View>
+                </View>
+                <TouchableOpacity style={styles.sectionbox} onPress={onAddButtonPress}>
+                    <Text style={styles.title}>Health</Text>
+                    <Text style={styles.text}>Find more about health resources and book appointments</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.sectionbox} onPress={onAddButtonPress}>
+                    <Text style={styles.title}>Finance</Text>
+                    <Text style={styles.text}>Find more about health resources and book appointments</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.sectionbox} onPress={onAddButtonPress}>
+                    <Text style={styles.title}>Housing</Text>
+                    <Text style={styles.text}>Find more about health resources and book appointments</Text>
                 </TouchableOpacity>
             </View>
-            { entities && (
-                <View style={styles.listContainer}>
-                    <FlatList
-                        data={entities}
-                        renderItem={renderEntity}
-                        keyExtractor={(item) => item.id}
-                        removeClippedSubviews={true}
-                    />
-                </View>
-            )}
-        </View>
+        </SafeAreaView>
     )
 }
+
+
+// export default function HomeScreen(props) {
+
+//     const [entityText, setEntityText] = useState('')
+//     const [entities, setEntities] = useState([])
+
+//     const entityRef = firebase.firestore().collection('entities')
+//     const userID = props.extraData.id
+
+//     useEffect(() => {
+//         entityRef
+//             .where("authorID", "==", userID)
+//             .orderBy('createdAt', 'desc')
+//             .onSnapshot(
+//                 querySnapshot => {
+//                     const newEntities = []
+//                     querySnapshot.forEach(doc => {
+//                         const entity = doc.data()
+//                         entity.id = doc.id
+//                         newEntities.push(entity)
+//                     });
+//                     setEntities(newEntities)
+//                 },
+//                 error => {
+//                     console.log(error)
+//                 }
+//             )
+//     }, [])
+
+//     const onAddButtonPress = () => {
+//         if (entityText && entityText.length > 0) {
+//             const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+//             const data = {
+//                 text: entityText,
+//                 authorID: userID,
+//                 createdAt: timestamp,
+//             };
+//             entityRef
+//                 .add(data)
+//                 .then(_doc => {
+//                     setEntityText('')
+//                     Keyboard.dismiss()
+//                 })
+//                 .catch((error) => {
+//                     alert(error)
+//                 });
+//         }
+//     }
+
+//     const renderEntity = ({item, index}) => {
+//         return (
+//             <View style={styles.entityContainer}>
+//                 <Text style={styles.entityText}>
+//                     {index}. {item.text}
+//                 </Text>
+//             </View>
+//         )
+//     }
+
+//     return (
+//         <View style={styles.container}>
+//             <View style={styles.formContainer}>
+//                 <TextInput
+//                     style={styles.input}
+//                     placeholder='Add new entity'
+//                     placeholderTextColor="#aaaaaa"
+//                     onChangeText={(text) => setEntityText(text)}
+//                     value={entityText}
+//                     underlineColorAndroid="transparent"
+//                     autoCapitalize="none"
+//                 />
+//                 <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
+//                     <Text style={styles.buttonText}>Add</Text>
+//                 </TouchableOpacity>
+//             </View>
+//             { entities && (
+//                 <View style={styles.listContainer}>
+//                     <FlatList
+//                         data={entities}
+//                         renderItem={renderEntity}
+//                         keyExtractor={(item) => item.id}
+//                         removeClippedSubviews={true}
+//                     />
+//                 </View>
+//             )}
+//         </View>
+//     )
+// }
+
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,56 +1,114 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+    // container: {
+    //     flex: 1,
+    //     alignItems: 'center'
+    // },
+    // formContainer: {
+    //     flexDirection: 'row',
+    //     height: 80,
+    //     marginTop: 40,
+    //     marginBottom: 20,
+    //     flex: 1,
+    //     paddingTop: 10,
+    //     paddingBottom: 10,
+    //     paddingLeft: 30,
+    //     paddingRight: 30,
+    //     justifyContent: 'center',
+    //     alignItems: 'center'
+    // },
+    // input: {
+    //     height: 48,
+    //     borderRadius: 5,
+    //     overflow: 'hidden',
+    //     backgroundColor: 'white',
+    //     paddingLeft: 16,
+    //     flex: 1,
+    //     marginRight: 5
+    // },
+    // button: {
+    //     height: 47,
+    //     borderRadius: 5,
+    //     backgroundColor: '#788eec',
+    //     width: 80,
+    //     alignItems: "center",
+    //     justifyContent: 'center'
+    // },
+    // buttonText: {
+    //     color: 'white',
+    //     fontSize: 16
+    // },
+    // listContainer: {
+    //     marginTop: 20,
+    //     padding: 20,
+    // },
+    // entityContainer: {
+    //     marginTop: 16,
+    //     borderBottomColor: '#cccccc',
+    //     borderBottomWidth: 1,
+    //     paddingBottom: 16
+    // },
+    // entityText: {
+    //     fontSize: 20,
+    //     color: '#333333'
+    // }
     container: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    formContainer: {
-        flexDirection: 'row',
-        height: 80,
-        marginTop: 40,
-        marginBottom: 20,
-        flex: 1,
-        paddingTop: 10,
-        paddingBottom: 10,
-        paddingLeft: 30,
-        paddingRight: 30,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    input: {
-        height: 48,
-        borderRadius: 5,
-        overflow: 'hidden',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
         backgroundColor: 'white',
-        paddingLeft: 16,
-        flex: 1,
-        marginRight: 5
     },
-    button: {
-        height: 47,
-        borderRadius: 5,
-        backgroundColor: '#788eec',
-        width: 80,
-        alignItems: "center",
-        justifyContent: 'center'
+    text: {
+        fontSize: 16,
+        color: 'black',
     },
-    buttonText: {
-        color: 'white',
-        fontSize: 16
+    header: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        alignContent: 'center',
+        color: 'black',
     },
-    listContainer: {
-        marginTop: 20,
+    title: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: 'black',
+    },
+    headerbox: {
+        width: '80%',
+        height: 200,
+        margin: 30,
         padding: 20,
+        alignItems: 'center',
+        backgroundColor: 'papayawhip',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#fff'
     },
-    entityContainer: {
-        marginTop: 16,
-        borderBottomColor: '#cccccc',
-        borderBottomWidth: 1,
-        paddingBottom: 16
+    favbox: {
+        width: '80%',
+        height: 100,
+        padding: 20,
+        backgroundColor: 'pink',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#fff',
+        display: 'flex',
+        flexDirection: 'row',
+        overflow: 'scroll',
     },
-    entityText: {
-        fontSize: 20,
-        color: '#333333'
-    }
+    cols: {
+        margin: 10,
+    },
+    sectionbox: {
+        width: '80%',
+        // height: 100,
+        backgroundColor: 'powderblue',
+        margin: 10,
+        padding: 20,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#fff'
+    },
+
 })
